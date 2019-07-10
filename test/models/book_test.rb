754db30a7b07c2332d2cecaae321d22662889abc
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2019-07-10 10:27:58 +0700

    :construction_worker: setup annotate model

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id          :bigint(8)        not null, primary key
+#  title       :string
+#  slug        :string
+#  author      :string
+#  cover       :string
+#  publisher   :string
+#  description :string
+#  genre       :string
+#  price       :decimal(, )      default(0.0)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
